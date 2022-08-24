e614117423f4e618981c8fbf3dd23837c550d0ff
Eric Ridgeway <ericridgeway@gmail.com>
2022-08-23 23:54:58 -0400

    Scaff

--- lib/function_label.ex
+++ lib/function_label.ex
@@ -1,18 +1,2 @@
 defmodule FunctionLabel do
-  @moduledoc """
-  Documentation for `FunctionLabel`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> FunctionLabel.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/function_label_test.exs
+++ test/function_label_test.exs
@@ -2,7 +2,4 @@ defmodule FunctionLabelTest do
   use ExUnit.Case
   doctest FunctionLabel
 
-  test "greets the world" do
-    assert FunctionLabel.hello() == :world
-  end
 end
